38e8f360ff2599a02a194c55f5efc7c5e5baafb0
Ashishumarao <ashishumarao.dev@gamil.com>
2024-07-19 01:09:16 +0530

    first commit

---
+++ project.html/script.js
@@ -0,0 +1,11 @@
+/** @format */
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('#main'),
+  smooth: true,
+});
+function circleMouse(){
+    window.addEventListener("mousemove",function(details))
+    document.querySelector("#mincircle").style.transform=`translate(${details.clientX}px,${details.clientY})`
+}
+circleMouse();
